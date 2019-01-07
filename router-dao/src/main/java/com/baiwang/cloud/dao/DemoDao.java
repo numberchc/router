efcc2161a5100f3fd7be06dce3c7fd3c0f66b507
chenhuichang <chenhuichang@baiwang.com>
2019-01-07 14:31:27 +0800

    第一次提交：代码初始

---
+++ router-dao/src/main/java/com/baiwang/cloud/dao/DemoDao.java
@@ -0,0 +1,19 @@
+package com.baiwang.cloud.dao;
+
+
+/**
+ * 模板测试接口，可以删除
+ * 
+ * @author wuyuegang
+ * 
+ */
+public interface DemoDao
+{
+    /**
+     * get router list from db
+     * 
+     * @return
+     */
+    public String getProducts();
+    
+}
